dc4bb87f9d01cc64c44fdf0f9277e78cee66f2f0
jaherrera01 <65740999+jaherrera01@users.noreply.github.com>
2020-11-25 15:13:00 -0500

    *se modifico la extencion ComprasController.php *se modifico el archivo testCompras.php

---
+++ app/Controllers/ComprasController.php
@@ -0,0 +1,177 @@
+<?php
+namespace App\Controllers;
+
+require(__DIR__ . '/../../vendor/autoload.php'); //Requerido para convertir un objeto en Array
+require_once(__DIR__ . '/../Models/Compras.php');
+require_once(__DIR__ . '/../Models/Personas.php');
+require_once(__DIR__ . '/../Models/GeneralFunctions.php');
+
+use App\Models\Compras;
+use App\Models\GeneralFunctions;
+use App\Models\Ventas;
+use Carbon\Carbon;
+
+if (!empty($_GET['action'])) { //ComprasController.php?action=create
+    ComprasController::main($_GET['action']);
+}
+
+class ComprasController
+{
+
+    static function main($action)
+    {
+        if ($action == "create") {
+            ComprasController::create();
+        } else if ($action == "edit") {
+            ComprasController::edit();
+        } else if ($action == "searchForID") {
+            ComprasController::searchForID($_REQUEST['idCompras']);
+        } else if ($action == "searchAll") {
+            ComprasController::getAll();
+        } else if ($action == "Procesada") {
+            ComprasController::inactivate();
+        } else if ($action == "Pendiente") {
+            ComprasController::activate();
+        }
+    }
+
+    static public function create()
+    {
+        try {
+            $arrayCompras = array();
+            $arrayCompras['fecha'] = Carbon::parse($_POST['fecha']);
+            $arrayCompras['valor_total'] = $_POST['valor_total'];
+            $arrayCompras['persona_id'] = $_POST['persona_id'];
+            $arrayCompras['estado'] = $_POST['estado'];
+
+            //esta linea aun no funciona debemos dejar lo de registrar por otro tipo de dato no se puede por id
+            //if (!Compras::ComprasRegistrada($arrayCompras['id'])) {
+            $Compras = new Compras($arrayCompras);
+            if ($Compras->save()) {
+                header("Location: ../../views/modules/compras/index.php?accion=create&respuesta=correcto");
+            }
+            /*} else {
+                header("Location: ../../views/modules/compras/create.php?respuesta=error&mensaje=Venta ya registrada");
+            }*/
+        } catch (Exception $e) {
+            GeneralFunctions::console($e, 'error', 'errorStack');
+            //header("Location: ../../views/modules/usuarios/create.php?respuesta=error&mensaje=" . $e->getMessage());
+        }
+    }
+
+
+    static public function edit()
+    {
+        try {
+            $arrayCompras = array();
+            $arrayCompras['fecha'] = Carbon::parse($_POST['fecha']);
+            $arrayCompras['valor_total'] = $_POST['valor_total'];
+            $arrayCompras['persona_id'] = $_POST['persona_id'];
+            $arrayCompras['estado'] = $_POST['estado'];
+            $arrayCompras['id'] = $_POST['id'];
+
+            $Compras = new Compras($arrayCompras);
+            $Compras->update();
+
+            header("Location: ../../views/modules/compras/show.php?id=" . $Compras->getId() . "&respuesta=correcto");
+        } catch (\Exception $e) {
+            GeneralFunctions::console($e, 'error', 'errorStack');
+            //header("Location: ../../views/modules/compras/edit.php?respuesta=error&mensaje=".$e->getMessage());
+        }
+    }
+
+    static public function searchForID($id)
+    {
+        try {
+            return Compras::searchForId($id);
+        } catch (\Exception $e) {
+            GeneralFunctions::console($e, 'error', 'errorStack');
+            //header("Location: ../../views/modules/compras/manager.php?respuesta=error");
+        }
+    }
+
+    static public function getAll()
+    {
+        try {
+            return Compras::getAll();
+        } catch (\Exception $e) {
+            GeneralFunctions::console($e, 'log', 'errorStack');
+            //header("Location: ../Vista/modules/compras/manager.php?respuesta=error");
+        }
+    }
+
+
+    static public function selectCompras($isMultiple = false,
+                                         $isRequired = true,
+                                         $id = "compras_id",
+                                         $nombre = "compras_id",
+                                         $defaultValue = "",
+                                         $class = "form-control",
+                                         $where = "",
+                                         $arrExcluir = array())
+    {
+        $arrCompras = array();
+        if ($where != "") {
+            $base = "SELECT * FROM `h&mcomputadores`.Compras WHERE ";
+            $arrCompras = Compras::search($base . ' ' . $where);
+        } else {
+            $arrCompras = Compras::getAll();
+        }
+
+        $htmlSelect = "<select " . (($isMultiple) ? "multiple" : "") . " " . (($isRequired) ? "required" : "") . " id= '" . $id . "' name='" . $nombre . "' class='" . $class . "' style='width: 100%;'>";
+        $htmlSelect .= "<option value='' >Seleccione</option>";
+        if (count($arrCompras) > 0) {
+            /* @var $arrCompras Ventas[] */
+            foreach ($arrCompras as $Compras)
+                if (!VentasController::comprasIsInArray($Compras>getId(), $arrExcluir))
+                    $htmlSelect .= "<option " . (($Compras != "") ? (($defaultValue == $Compras->getId()) ? "selected" : "") : "") . " value='" . $Compras->getId() . "'>" .  $Compras->getFecha() . "</option>";
+        }
+        $htmlSelect .= "</select>";
+        return $htmlSelect;
+    }
+
+    private static function comprasIsInArray($idCompras, $arrCompras)
+    {
+        if (count($arrCompras) > 0) {
+            foreach ($arrCompras as $Compras) {
+                if ($Compras->getId() == $idCompras) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static public function inactivate()
+    {
+        try {
+            $ObjCompras = Compras::searchForId($_GET['Id']);
+            $ObjCompras->setEstado("Procesada");
+            if ($ObjCompras->update()) {
+                header("Location: ../../views/modules/compras/index.php");
+            } else {
+                header("Location: ../../views/modules/compras/index.php?respuesta=error&mensaje=Error al guardar");
+            }
+        } catch (\Exception $e) {
+            GeneralFunctions::console($e, 'error', 'errorStack');
+            //header("Location: ../../views/modules/Compras/index.php?respuesta=error&mensaje=".$e->getMessage());
+        }
+    }
+
+
+    static public function activate()
+    {
+        try {
+            $ObjCompras = Compras::searchForId($_GET['Id']);
+            $ObjCompras->setEstado("Pendiente");
+            if ($ObjCompras->update()) {
+                header("Location: ../../views/modules/compras/index.php");
+            } else {
+                header("Location: ../../views/modules/compras/index.php?respuesta=error&mensaje=Error al guardar");
+            }
+        } catch (\Exception $e) {
+            GeneralFunctions::console($e, 'error', 'errorStack');
+            //header("Location: ../../views/modules/compras/index.php?respuesta=error");
+        }
+    }
+}
---
+++ app/Models/Compras.php
@@ -0,0 +1,215 @@
+<?php
+namespace App\Models;
+
+require_once (__DIR__ .'/../../vendor/autoload.php');
+require_once ('Personas.php');
+require_once  ('BasicModel.php');
+
+use App\Models\Personas;
+use Carbon\Carbon;
+
+class Compras extends BasicModel
+{
+    //Propiedades
+    protected int $id;
+    protected Carbon $fecha;
+    protected float $valor_total;
+    protected Personas $persona_id;
+    protected string $estado;
+
+
+    public function __construct($arrCompras = array())
+    {
+        //Propiedad recibida y asigna a una propiedad de la clase
+        parent::__construct();
+        $this->id = $arrCompras['id'] ?? 0;
+        $this->fecha = $arrCompras['fecha'] ?? new Carbon();
+        $this->valor_total = $arrCompras['valor_total'] ?? 0;
+        $this->persona_id = !empty($arrCompras['persona_id']) ? Personas::searchForId($arrCompras['persona_id']) : new Personas();
+        $this->estado = $arrCompras['estado'] ?? '';
+    }
+
+    public function __destruct() // Cierro Conexiones
+    {
+        /*
+        echo "<span style='color: #8b0000'>";
+        echo $this->getNombre()." se ha eliminado<br/>";
+        echo "</span>";
+         */
+    }
+
+    /**
+     * @return int|mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return Carbon|mixed
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * @param Carbon|mixed $fecha
+     */
+    public function setFecha(Carbon $fecha): void
+    {
+        $this->fecha = $fecha;
+    }
+
+    /**
+     * @return float|int|mixed
+     */
+    public function getValorTotal()
+    {
+        return $this->valor_total;
+    }
+
+    /**
+     * @param float|int|mixed $valor_total
+     */
+    public function setValorTotal($valor_total): void
+    {
+        $this->valor_total = $valor_total;
+    }
+
+    /**
+     * @return \App\Models\Personas|mixed
+     */
+    public function getPersonaId()
+    {
+        return $this->persona_id;
+    }
+
+    /**
+     * @param \App\Models\Personas|mixed $persona_id
+     */
+    public function setPersonaId($persona_id): void
+    {
+        $this->persona_id = $persona_id;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param mixed|string $estado
+     */
+    public function setEstado($estado): void
+    {
+        $this->estado = $estado;
+    }
+
+
+    public function save(): Compras
+    {
+        $result = $this->insertRow("INSERT INTO `h&mcomputadores`.Compras VALUES (NULL, ?, ?, ?, ?)", array(
+                $this->fecha->toDateString(), //YYYY-MM-DD,
+                $this->valor_total,
+                $this->persona_id->getId(),
+                $this->estado
+            )
+        );
+        $this->setId(($result) ? $this->getLastId() : null);
+        $this->Disconnect();
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function update()
+    {
+        $result = $this->updateRow("UPDATE `h&mcomputadores`.Compras SET fecha = ?, valor_total = ?, persona_id= ?,  estado = ? WHERE id = ?", array(
+                $this->fecha->toDateString(),
+                $this->valor_total,
+                $this->persona_id->getId(),
+                $this->estado,
+                $this->id
+            )
+        );
+        $this->Disconnect();
+        return $result;
+    }
+
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function deleted($id)
+    {
+        $Compras = Compras::searchForId($id); //Buscando un Municipio por el ID
+        $Compras->setEstado("Pendiente"); //Cambia el estado del Usuario
+        return $Compras->update();                    //Guarda los cambios..
+    }
+
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+
+    public static function search($query): array
+    {
+        $arrCompras = array();
+        $tmp = new Compras();
+        $getrows = $tmp->getRows($query);
+
+        foreach ($getrows as $deta) {
+            $Compras = new Compras();
+            $Compras->id = $deta['id'];
+            $Compras->fecha = Carbon::parse($deta['fecha']);
+            $Compras->valor_total = $deta['valor_total'];
+            $Compras->persona_id = Personas::searchForId($deta['persona_id']);
+            $Compras->estado = $deta['estado'];
+            array_push($arrCompras, $Compras);
+        }
+        $tmp->Disconnect();
+        return $arrCompras;
+    }
+
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public static function searchForId($id)
+    {
+        $Compras = null;
+        if ($id > 0) {
+            $Compras = new Ventas();
+            $getrow = $Compras->getRow("SELECT * FROM `h&mcomputadores`.Compras WHERE id =?", array($id));
+            $Compras->id = $getrow['id'];
+            $Compras->fecha = Carbon::parse($getrow['fecha']);
+            $Compras->valor_total = $getrow['valor_total'];
+            $Compras->persona_id = Personas::searchForId($getrow['persona_id']);
+            $Compras->estado = $getrow['estado'];
+        }
+        $Compras->Disconnect();
+        return $Compras;
+    }
+
+    public static function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+}
